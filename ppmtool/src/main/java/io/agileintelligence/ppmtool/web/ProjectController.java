1bef0c0ae3d53de0ebbc3ae6b4b2528146c05bf3
steven.yeo <steven.yeo@velocity-solutions.com>
2021-09-26 15:36:10 +0700

    branch6 progress

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
 
 @RestController
@@ -26,19 +27,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
-		if (result.hasErrors()) {
-			
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for (FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		
+		if (errorMap != null) return errorMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
