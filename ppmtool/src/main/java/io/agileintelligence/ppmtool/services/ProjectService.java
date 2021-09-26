87208399a4c873b740f14a9d426355a016b1fed3
steven.yeo <steven.yeo@velocity-solutions.com>
2021-09-26 16:30:47 +0700

    branch7 progress

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -14,6 +15,11 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project project) {
 		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+		}
 	}
 }
