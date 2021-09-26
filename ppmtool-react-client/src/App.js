ba2c5d9ff720153f37e3c8634452014d48204605
steven.yeo <steven.yeo@velocity-solutions.com>
2021-09-26 21:35:54 +0700

    branch16 Progress

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
